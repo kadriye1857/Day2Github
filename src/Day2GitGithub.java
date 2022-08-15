8d8e8c91415642dc656a274314f9816682794c9b
Kadriye Unal <kadriye1857@gmail.com>
2022-08-15 22:58:42 +0300

    V3

--- src/Day2GitGithub.java
+++ src/Day2GitGithub.java
@@ -18,5 +18,6 @@ public class Day2GitGithub {
      */
     public static void main(String[] args) {
         System.out.println("Branch deneme 1");
+        System.out.println("Master deneme2");
     }
 }
