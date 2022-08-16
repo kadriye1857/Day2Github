7b6d030b6f92f8254303e4d4f707bf43fabd05a8
Kadriye Unal <kadriye1857@gmail.com>
2022-08-16 16:22:01 +0300

    1.C

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -1,8 +1,5 @@
 public class Day1Git {
     public static void main(String[] args) {
-        System.out.println(" Hello Git 1. Deneme");
-        System.out.println("Hello Git 2. Deneme");
-        System.out.println("Hello Git 3. Denemegit ");
-        System.out.println("Hello Git 4. Deneme");
+        System.out.println("Hello Git 1. Deneme");
     }
 }
